488a8cb0843d57afd4048181b4654f9bb12530ae
Campsiyas <campsiyasicloud@gmail.com>
2025-03-08 18:02:21 -0800

    plantilla
    inicio de algo hermoso

---
+++ formpost.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+Hola <?php echo $_POST["nombre"]; ?><br>
+tu codigo es: <?php echo $_POST["codigo"]; ?><br>
+Tu servicio es: <?php echo $_POST["servicio"]; ?><br>
+<?php echo $_POST["comentarios"]; ?><br>
+tu equipo estara a mas tardar 4 dias despues de hoy
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form action="formpost.php" method="post">
+    Codigo: <input type="number" name="codigo"><br>
+    Nombre: <input type="text" name="nombre"><br>
+    Telefono: <input type="text" name="telefono"><br>
+    Email: <input type="text" name="email"><br>
+    Servicio: <input type="text" name="servicio"><br>
+    Comentarios: <input type="text" name="comentarios"><br>
+    <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
