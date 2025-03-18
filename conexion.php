6c3579d87c8b2c405b7f239b3675e7047cb41556
Campsiyas <campsiyasicloud@gmail.com>
2025-03-18 14:18:58 -0600

    base de datos

--- conexion.php
+++ conexion.php
@@ -1,16 +1,55 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$pass = "";
+$servidor = "localhost";
+$usuario = "root";
+$clave = "";
 $db = "cel";
 
-$conexion = new mysqli($server, $user, $pass, $db);
+$enlace = mysqli_connect($servidor, $usuario, $clave, $db);
 
-if ($conexion->connect_errno){
-    die("Conexion Fallida" . $conexion->connect_errno);
-} else {
-  echo "conectado";
+?>
+
+<html>
+<head>
+  <link rel="stylesheet" href="estilo.css">
+    <title>Libertad financiera</title>
+</head>
+<body>
+
+<form action="#" method="post">
+  <div class="tarjeta">
+    <h1>Completa</h1>
+    <!-- <input type="number" name="codigo" placeholder="Codigo:"><br> -->
+    <input type="text" name="nombre" placeholder="Nombre:"><br>
+    <input type="text" name="telefono" placeholder="Telefono:"><br>
+    <input type="email" name="email" placeholder="Email:"><br>
+    <select id="cmbMake" name="servicio" placeholder="Servicio:">
+    <option value="">Selecciona un servicio</option>
+    <option value="Cambio de pantalla">Cambio de pantalla</option>
+    <option value="Formateo">Formateo</option>
+    <option value="Liberacion">Liberacion</option>
+    </select><br>
+    <input type="text" name="comentarios" placeholder="Comentarios:"><br>
+    <input type="submit" name=registro value="Enviar">
+    </div>
+
+</form>
+</body>
+</html>
+
+
+<?php
+if (isset($_POST['registro'])) {
+  $nombre= $_POST ['nombre'];
+  $telefono= $_POST ['telefono'];
+  $email= $_POST ['email'];
+  $servicio= $_POST ['servicio'];
+  $comentarios= $_POST ['comentarios'];
+
+  $insert="INSERT INTO datos VALUES ('','$nombre','$telefono','$email','$servicio','$comentarios','')";
+
+  $ejecutar = mysqli_query ($enlace,$insert);
+  header("Location: formpost.php");
+  exit();
 }
-
 ?>
--- termica.php
+++ termica.php
@@ -12,19 +12,14 @@
             border: 1px solid #000;
             margin: 20px auto;
             font-family: Arial, sans-serif;
-            
         }
     </style>
 </head>
 <body>
   <div class="tarjeta">
     <div id="ticket">
-        <h2>CampasCorp</h2>
-        <p>Hola <?php echo $_POST["nombre"]; ?></p>
-        <p>tu codigo es: <?php echo $_POST["codigo"]; ?></p>
-        <p><?php echo $_POST["servicio"]; ?></p>
-        <p><?php echo $_POST["comentarios"]; ?></p>
-        <p>tu equipo estara a mas tardar 4 dias despues de hoy</p>
+
+        <p>1</p>
 
     </div>
     <button onclick="imprimirTicket()">Imprimir Ticket</button>
